5a17f2537ec263101d32b2a2b0d42071b3c33577
mrAjay1 <ajayprakash0320@gmail.com>
2025-01-21 12:55:06 +0530

    feat: review schema

--- src/main/java/mrajay1/online/movieApp/Movies/Movies.java
+++ src/main/java/mrajay1/online/movieApp/Movies/Movies.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
 
@@ -22,4 +23,6 @@ public class Movies {
     private String trailerLink;
     private String poster;
     private List<String> genres;
+    @DocumentReference
+    private List<Reviews> reviews;
 }
---
+++ src/main/java/mrajay1/online/movieApp/Movies/Reviews.java
@@ -0,0 +1,18 @@
+package mrajay1.online.movieApp.Movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "reviews")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reviews {
+    @Id
+    private ObjectId _id;
+    private String review;
+}
